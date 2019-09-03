617872f7ffb9bc6c9a3202a089aef28640ef1c0d
Todor Todorov <ttodorov@null.net>
2019-09-03 22:03:04 +0300

    Docs: improve documentation of core/libs
    Docstrings improved, comments added

--- api_example/core/lib/flask_extensions/flask_sqlite3.py
+++ api_example/core/lib/flask_extensions/flask_sqlite3.py
@@ -1,10 +1,39 @@
+"""
+sqlite3 flask extension, taken from the flask documentation
+and improved
+"""
 import sqlite3
 from flask import current_app, _app_ctx_stack
 
 
+def make_dicts(cursor, row):
+    """
+    Helper to make dicts from sqlite3 rows
+    """
+    return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
+
+
 class ExtendedConnection(sqlite3.Connection):
+    """
+    Subclassing original sqlite3.Connection and adding
+
+    # methods for easy quering, the idea taken from
+    # https://postgres-py.readthedocs.io/en/latest/
+    # https://github.com/3kwa/sql
+    # https://github.com/kennethreitz/records
+        con.one(..)
+        con.all(..)
+
+    # spatialite initialization
+        con.enable_spatialite()
+    """
 
     def all(self, query, args=()):
+        """
+        Method accepts sql query and returns all rows
+        Implemented as a generator
+        """
         cur = self.execute(query, args)
         while True:
             row = cur.fetchone()
@@ -14,6 +43,9 @@ class ExtendedConnection(sqlite3.Connection):
         cur.close()
 
     def one(self, query, args=()):
+        """
+        Method takes sql and returns one row
+        """
         cur = self.execute(query, args)
         rv = cur.fetchall()
         cur.close()
@@ -22,17 +54,18 @@ class ExtendedConnection(sqlite3.Connection):
         return None
 
     def enable_spatialite(self):
+        """
+        Enables spatialite extension on the current connection
+        """
         self.enable_load_extension(True)
         self.execute('SELECT load_extension("mod_spatialite.so")')
         # self.execute('SELECT InitSpatialMetaData(1);')
 
 
-def make_dicts(cursor, row):
-    return dict((cursor.description[idx][0], value)
-                for idx, value in enumerate(row))
-
-
 class SQLite3(object):
+    """
+    sqlite3 flask plugin as described in flask documentation and improved
+    """
     def __init__(self, app=None):
         self.app = app
         if app is not None:
@@ -43,8 +76,10 @@ class SQLite3(object):
         app.teardown_appcontext(self.teardown)
 
     def connect(self):
+        # using extended connection class as described above
         con = sqlite3.connect(current_app.config['SQLITE3_DATABASE'],
                               factory=ExtendedConnection)
+        # data returned to dicts
         con.row_factory = make_dicts
         return con
 
@@ -55,6 +90,11 @@ class SQLite3(object):
 
     @property
     def con(self):
+        """
+        This is the connection (for short con) to be used in the code
+        like this:
+        db.con.execute(...), db.con.execscript(..), etc.
+        """
         ctx = _app_ctx_stack.top
         if ctx is not None:
             if not hasattr(ctx, 'sqlite3_db'):
--- api_example/core/lib/validators/validate_with_decorator.py
+++ api_example/core/lib/validators/validate_with_decorator.py
@@ -1,10 +1,36 @@
+"""
+@validate_with(...) decorator
+"""
 from functools import wraps
 from flask import abort
 
-# TODO: how to connect with api without giving api as argument?
-
 
 class validate_with:
+    """
+    @validate_with(...) decorator
+    To be used with flask restplus to decorate the view methods:
+
+        ....
+
+        @api.marshal_with(marshaler_some)
+        @api.expext(some_marshaler)
+        @validate_with(api, validation_callback)
+        def post(self, id):
+            ....
+
+    Parameters:
+        api: current api object
+        validator: any callable object to validate the payload. The payload is
+                   given as first argument
+
+    Returns:
+        None, but aborts connection on exception in validator
+
+    Validator development:
+        with signature def somevalidator(payload):
+        should raise exception on invalid payload
+    """
+    # TODO: how to interface with api without giving api as argument?
 
     def __init__(self, api, validator):
         self.api = api
@@ -14,7 +40,10 @@ class validate_with:
         @wraps(func)
         def wrapper(*arg, **kwarg):
             try:
+                # validation with the given validator (should raise exception
+                # on invalid payload)
                 self.validator(self.api.payload)
+                # the actual function is also enclosed in try-except
                 result = func(*arg, **kwarg)
             except Exception as e:
                 abort(400, str(e))
